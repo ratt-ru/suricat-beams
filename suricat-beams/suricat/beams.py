4c27ced784c98b1b55112d75c9b700120377fcb1
Ayanabha De <rik@Ayanabhas-MacBook-Air.local>
2025-07-25 04:40:37 +0530

    New Feature: Added a new class named GMRT_BeamWizard that provides analytical uGMRT beams

--- suricat-beams/suricat/beams.py
+++ suricat-beams/suricat/beams.py
@@ -258,7 +258,7 @@ class BeamWizard(object):
         fy = np.meshgrid(freq, xpyp[1], indexing='ij')   # mesh freq,y 
         coords = np.vstack([fy] + [fx[1:]])  # mesh freq,yx
         return map_coordinates(self._get_prefilter(var, i, j), coords, prefilter=True)
-
+    
     def get_time_variable_beamgain(self, srcpos: SkyCoord, mjds: List[float], 
                                         loc: EarthLocation = None, spi: Optional[float] = None):
         xpyp = self.get_source_pixel_coodinates(srcpos, mjds, loc)
@@ -272,5 +272,105 @@ class BeamWizard(object):
             spectral_weights = (self.freqs/self.freqs[0])**spi
             weights = self.band_weights*spectral_weights
             return (bg_freq*weights[:,np.newaxis]).sum(axis=0) / weights.sum(), xpyp
+            
+            
+class GMRT_BeamWizard(object):
+    """Attaches to a BDS and provides various convenienece functions
+    Eband: np.ndarray          # per-band power beam
+    Emean: np.ndarray          # mean MFS beam
+    band_weights: np.ndarray   # per-band weights
+    freqs: np.ndarray          # band frequencies
+    x0: int       # center pixel of beam
+    y0: int
+    delta: float  # degrees per pixel
+    ra0: float    # field centre in degrees
+    dec0: float"""
+
+    def __init__(self, image_name: str):
+        from . import log
+        self.log = log
+                 
+        if image_name.endswith(".fits"):
+            log.info(f"obtaining WCS from FITS image {image_name}")
+            fitshdr = fits.open(image_name)[0].header
+            self.wcs = WCS(fitshdr)
+            self.time = None
+        elif image_name.endswith(".zarr") or image_name.endswith(".zarr/"):
+            log.info(f"obtaining WCS from dataset {image_name}")
+            ds = xarray.open_zarr(image_name)
+            fitshdr = fits.Header(dict(ds.attrs['fits_header']))
+            self.wcs = WCS(fitshdr)
+            self.times = Time(ds.coords['TIME'].values/(24*3600), format='mjd')
+            log.info(f"time axis is {self.times[0].iso} to {self.times[-1].iso}")
+        else:
+            raise RuntimeError(f"unable to determine type of image {image_name}")
+        # drop WCS axes >2
+        while len(self.wcs.axis_type_names) > 2:
+            log.debug(f"dropping WCS axis {self.wcs.axis_type_names[-1]}")
+            self.wcs = self.wcs.dropaxis(len(self.wcs.axis_type_names) - 1)
+        self.centre = self.wcs.pixel_to_world(fitshdr['CRPIX1'] - 1, fitshdr['CRPIX2'] - 1)
+        log.info(f"image centre is at {self.centre}")
+        # location could be made configurable        
+        self.default_location = EarthLocation.of_site("GMRT") 
+        log.info(f"location is GMRT ({self.default_location})")
+
+    def get_source_coodinates(self, srcpos: SkyCoord, 
+                                    times: Optional[Time] = None, 
+                                    loc: EarthLocation = None,
+                                    signs=(1,1), swap=False):
+        """
+        Given a sky position and a list of times, derives the in-beam coordinates of the source (in beam pixels)
+        """
+        if loc is None:
+            loc = loc or self.default_location
+        if times is None:
+            if self.times is None:
+                raise RuntimeError("explicit times must be supplied, since BeamWizard was constructed without observational time info")
+            times = self.times
+        # convert positions to alt-az
+        frame = AltAz(obstime=times, location=loc)       
+        altaz_src = srcpos.transform_to(frame)
+        altaz_centre = self.centre.transform_to(frame)
+        # get angle and separation of source w.r.t. centre
+        angles = altaz_centre.position_angle(altaz_src)
+        seps = altaz_centre.separation(altaz_src)
+        # convert to pixel position 
+        # confused about angles, but experiments show that 0 is up and +90 is right 
+        #x = signs[0] * seps.deg * np.sin(angles.rad)
+        #y = signs[1] * seps.deg * np.cos(angles.rad)
+        #if swap:
+        #    x, y = y, x
+        #xp = x / self.bds.attrs['dx']  + self.bds.attrs['x0']
+        #yp = y / self.bds.attrs['dy']  + self.bds.attrs['y0']
+        return seps, angles
+
+    def ugmrt_beam_poly(self, freqs: float, sep: np.array):
+        coeffs_b5 = [1.0, -2.614e-3, 27.594e-7, -13.268e-10,  2.395e-13]  # Band-5 coefficients
+        coeffs_b4 = [1.0, -3.263e-3, 42.618e-7, -25.580e-10,  5.823e-13]  # Band-4 coefficients
+        coeffs_b3 = [1.0, -3.129e-3, 38.816e-7, -21.608e-10,  4.483e-13]  # Band-3 coefficients
+        coeffs_b2 = [1.0, -3.089e-3, 39.314e-7, -23.011e-10,  5.037e-13]  # Band-2 coefficients
+        if freqs>=125 and freqs <=250:
+            coeffs = coeffs_b2
+        elif freqs>=250 and freqs<=500:
+            coeffs = coeffs_b3
+        elif freqs>=550 and freqs<=850:
+            coeffs = coeffs_b4
+        elif freqs>=1050 and freqs<=1450:
+            coeffs = coeffs_b5
+        else:
+            raise RuntimeError("GMRT band could not be recognised")
+
+        #sep = Angle(sep,unit='deg')
+        
+        freq_GHz = freqs*1e-3 # Expecting frequency input in MHz
+        sep_arcmin = sep.arcmin
+        r = sep_arcmin * freq_GHz
+
+        pb = np.zeros_like(r)
+        for i, c in enumerate(coeffs):
+            pb += c * r**(2 * i)  #analytical 8th order polynomial for uGMRT
+
+        return pb
+
 
 
